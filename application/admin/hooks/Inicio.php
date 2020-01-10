381e6ac83e04103074a45ea5c37115e9152869f5
mario <desarrollo2@c807.com>
2020-01-10 11:50:27 -0600

    Correcciones en la validacion de session

--- application/admin/hooks/Inicio.php
+++ application/admin/hooks/Inicio.php
@@ -9,37 +9,34 @@ class Inicio
     {
     	$this->ci =& get_instance();
     	$this->ci->load->helper(['jwt', 'authorization']);
-    	$this->libres = ['/usuario/login.json'];
+    	$this->libres = ['/resttouch/index.php/usuario/login.json'];
     }
 
     public function verificarSesion()
     {
-        if(!in_array($_SERVER['PATH_INFO'], $this->libres)) {
+        if(!in_array($_SERVER['REQUEST_URI'], $this->libres)) {
         	$headers = $this->ci->input->request_headers();
         	$response = ['mensaje' => '¡Acceso no autorizado!', 'valido' => false];
         	$continuar = true;
 			if (array_key_exists('Authorization', $headers)) {
 				$token = $headers['Authorization'];
-			} else {
-				$continuar = false;
-			}
-
-			try {
-				$data = AUTHORIZATION::validateToken($token);
-				if ($data === false) {
-					$continuar = false;
-				} else {
-					$now = new DateTime(date('Y-m-d H:i:s'));
-					$hasta = new DateTime($data->hasta);
-					$interval = $now->diff($hasta);
-					$diff = $interval->format('%a');
-
-					if ($now->format('Y-m-d H:i:s') > $hasta->format('Y-m-d H:i:s')) {
-						$response['mensaje'] = 'El token ya se venció. Debe loggearse de nuevo, por favor.';
+				try {
+					$data = AUTHORIZATION::validateToken($token);
+					if ($data === false) {
 						$continuar = false;
+					} else {
+						$now = new DateTime(date('Y-m-d H:i:s'));
+						$hasta = new DateTime($data->hasta);						
+
+						if ($now->format('Y-m-d H:i:s') > $hasta->format('Y-m-d H:i:s')) {
+							$response['mensaje'] = 'El token ya se venció. Debe loggearse de nuevo, por favor.';
+							$continuar = false;
+						}
 					}
+				} catch (Exception $e) {
+					$continuar = false;
 				}
-			} catch (Exception $e) {
+			} else {
 				$continuar = false;
 			}
 
