961da9b770fb4a8196d230589a4e227dabef18bb
JAragon <jaragon@spcdatapro.com>
2020-01-17 10:12:33 -0600

    Actualización del ER. 17/01/2020 10:12.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'admin', loadChildren:'./admin/admin.module#AdminModule' },
   { path: 'restaurante', loadChildren:'./restaurante/restaurante.module#RestauranteModule' },
+  { path: 'wms', loadChildren:'./wms/wms.module#WmsModule' },
   { path: '', redirectTo: 'admin/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
--- web/src/app/wms/wms-routing.module.ts
+++ web/src/app/wms/wms-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthguardService as AuthGuard } from '../admin/services/authguard.service';
 
+import { IngresoComponent } from './components/ingreso/ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ingresos', component: IngresoComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
