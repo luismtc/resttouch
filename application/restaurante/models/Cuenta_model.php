49fc9c4be9587360d12a1d47e8ae491453cd85eb
mario <desarrollo2@c807.com>
2020-01-27 11:46:29 -0600

    se elimino campo forma de pago

--- application/restaurante/models/Cuenta_model.php
+++ application/restaurante/models/Cuenta_model.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cuenta_model extends General_Model {
 
 	public $cuenta;
-	public $forma_pago;
 	public $comanda;
 	public $nombre;
 
