f4a582703579edf1e2bf42e84f82caff2f05a6ee
JAragon <jaragon@spcdatapro.com>
2020-02-21 08:18:32 -0600

    Agregar idusuario al token que se envía para las peticiones. 21/02/2020 08:18.

--- application/admin/models/Usuario_model.php
+++ application/admin/models/Usuario_model.php
@@ -31,6 +31,7 @@ class Usuario_model extends CI_Model
             if (isset($dbusr)) {
                 if (password_verify($credenciales['pwd'], $dbusr->contrasenia)) {
                     $tokenData = array(
+                        'idusuario' => $dbusr->usuario,
                         'usuario' => $credenciales['usr'],
                         'inicia' => date('Y-m-d H:i:s'),
                         'hasta' => date('Y-m-d H:i:s', strtotime('+12 hours'))
