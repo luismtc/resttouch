63dd5998501f5afcf013f5e9393857bffa918067
JAragon <jaragon@spcdatapro.com>
2020-03-24 19:43:23 -0600

    Inicio de eliminación del grid de bootstrap. 24/03/2020 19:43.

--- web/src/app/wms/components/ingreso/ingreso/ingreso.component.ts
+++ web/src/app/wms/components/ingreso/ingreso/ingreso.component.ts
@@ -17,29 +17,30 @@ export class IngresoComponent implements OnInit {
   @ViewChild('lstIngreso', { static: false }) lstIngresoComponent: ListaIngresoComponent;
   @ViewChild('frmIngreso', { static: false }) frmIngreso: FormIngresoComponent;
   public breakpoint: number = 2;
-  public tamFila: number;
+  // public tamFila: number;
 
   constructor(
     private ls: LocalstorageService
   ) {
     this.ingreso = { 
-      ingreso: null, tipo_movimiento: null, fecha: moment().format(GLOBAL.dbDateFormat), bodega: null, usuario: (this.ls.get(GLOBAL.usrTokenVar).idusr || 0), comentario: null, proveedor: null
+      ingreso: null, tipo_movimiento: null, fecha: moment().format(GLOBAL.dbDateFormat), bodega: null,
+      usuario: (this.ls.get(GLOBAL.usrTokenVar).idusr || 0), comentario: null, proveedor: null
     };
   }
 
   ngOnInit() {
     this.breakpoint = (window.innerWidth <= 360) ? 1 : 2;
-    this.tamFila = window.innerHeight - 64;
-    //console.log(this.tamFila);
+    // this.tamFila = window.innerHeight - 64;
+    // console.log(this.tamFila);
   }
 
   onWindowResize = (ev: any) => {
-    this.breakpoint = (ev.target.innerWidth <= 360) ? 1 : 2
-    this.tamFila = ev.target.innerHeight - 64;
-    //console.log(this.tamFila);
-  };
+    this.breakpoint = (ev.target.innerWidth <= 360) ? 1 : 2;
+    // this.tamFila = ev.target.innerHeight - 64;
+    // console.log(this.tamFila);
+  }
 
-  setIngreso = (ing: Ingreso) => { 
+  setIngreso = (ing: Ingreso) => {
     this.ingreso = ing;
     this.frmIngreso.loadDetalleIngreso(+this.ingreso.ingreso);
   }
