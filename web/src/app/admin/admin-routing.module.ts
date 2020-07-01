604d735348b5c3198c37b4cab8d2f9f998140eaa
mario <desarrollo2@c807.com>
2020-07-01 11:16:12 -0600

    se elimino ruta de propina

--- web/src/app/admin/admin-routing.module.ts
+++ web/src/app/admin/admin-routing.module.ts
@@ -7,7 +7,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsuarioComponent } from './components/usuario/usuario/usuario.component';
 import { ClienteComponent } from './components/cliente/cliente/cliente.component';
 import { MedidaComponent } from './components/medida/medida/medida.component';
-import { PropinaComponent } from './components/propina/propina/propina.component';
 import { PresentacionComponent } from './components/presentacion/presentacion/presentacion.component';
 
 const routes: Routes = [
@@ -17,7 +16,6 @@ const routes: Routes = [
   {path: 'cliente', component: ClienteComponent, canActivate: [AuthGuard]},
   {path: 'medida', component: MedidaComponent, canActivate: [AuthGuard]},
   {path: 'presentacion', component: PresentacionComponent, canActivate: [AuthGuard]},
-  {path: 'propina', component: PropinaComponent, canActivate: [AuthGuard]},
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
