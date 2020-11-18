31c036bfc3df58e7a105c03252fd48507d07488d
JAragon <jaragon@spcdatapro.com>
2020-11-18 17:08:55 -0600

    Agregar headers automáticos en cada app del backend para que no guarde caché. Actualización del modelo en forma_pago característica 'Sin factura'. 18/11/2020 17:08.

--- application/restaurante/hooks/Inicio.php
+++ application/restaurante/hooks/Inicio.php
@@ -69,6 +69,7 @@ class Inicio
 
     public function headers()
     {
+		header('Cache-Control: no-cache, no-store, max-age=0');
     	header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
 		header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
